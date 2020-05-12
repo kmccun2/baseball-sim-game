84005d784f49e180f81b404b240b5453773a4965
Kurt McCune <kurtm@performance.performance-contractors.com>
2020-05-12 07:54:51 -0500

    Version 1.0.0

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const fileUpload = require('express-fileupload')
+const path = require('path')
 
 const app = express()
 
@@ -22,5 +23,14 @@ app.post('/upload', (req, res) => {
     res.json({ fileName: file.name, filePath: `/uploads/${file.name}` })
   })
 })
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 app.listen(5000, () => console.log('Server Started...'))
