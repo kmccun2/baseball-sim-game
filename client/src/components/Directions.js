f6c24e31a804070e0efe9e7a633dc684c61fedf0
Kurt McCune <kurtm@performance.performance-contractors.com>
2020-05-12 14:43:57 -0500

    Fix 431 status issue and insert logo

---
+++ client/src/components/Directions.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Directions = () => {
+  return (
+    <div className='directions'>
+      <p>1. Go to gc.com and export your team's stats to CSV.</p>
+      <p>2. Click browse and upload the file.</p>
+      <p>3. Click on players to select your lineup.</p>
+      <p>
+        4. Select a number of games and innings per game you'd like to play.
+      </p>
+      <p>5. Play ball!</p>
+    </div>
+  )
+}
+
+export default Directions
