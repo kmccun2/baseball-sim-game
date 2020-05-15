b9e6c4b0da0fc65569168ab552f23d6b44e3c8f3
Kurt McCune <kurtm@performance.performance-contractors.com>
2020-05-14 22:36:46 -0500

    v1.0 Ready for deployment

---
+++ build/precache-manifest.2af943f10576643c05f6417ffb81bf49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0407601d6a00a3c4ee14",
+    "url": "/baseball-sim-game/static/css/main.4d38be9f.chunk.css"
+  },
+  {
+    "revision": "0407601d6a00a3c4ee14",
+    "url": "/baseball-sim-game/static/js/main.026ea708.chunk.js"
+  },
+  {
+    "revision": "32895df995bcfc10846b",
+    "url": "/baseball-sim-game/static/js/runtime~main.417c402c.js"
+  },
+  {
+    "revision": "df9cf1ceb2b19d82907a",
+    "url": "/baseball-sim-game/static/js/2.2164dd9a.chunk.js"
+  },
+  {
+    "revision": "5279250afabe68c119cee459cfca3b99",
+    "url": "/baseball-sim-game/static/media/logo.5279250a.png"
+  },
+  {
+    "revision": "fe78597288dd3db13b5332e2b98c786f",
+    "url": "/baseball-sim-game/index.html"
+  }
+];
